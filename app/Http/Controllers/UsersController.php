3d9b5cc77181c53e822dc358914a5dfe6c169442
unknown <harish.upworktry@gmail.com>
2022-11-03 20:25:27 +0530

    Simle aproach

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -24,6 +24,38 @@ class UsersController extends Controller{
      * @request
      * @return response 
     */
+    // public function deposite(Request $request){
+    //     $validator = Validator::make($request->all(), [
+    //         'owner_id' => 'required | numeric',
+    //         'owner_type' => 'required',
+    //         'balance' => 'required|numeric|between:0,99.99'
+    //     ]);
+    //     if($validator->fails()){
+    //         return response()->json($validator->errors(), 422);
+    //     }
+    //     try{
+    //         $user = User::find($request->owner_id);  
+    //         if($user){
+    //             if($request->balance >= 3 && $request->balance <= 100 ) {                    
+    //                 $user->wallet->deposit($request->balance); 
+    //                 return response()->json(['message' => 'Success! $'.$request->balance.' added to your wallet'] , 404);
+    //             }else{
+    //                 $message = 'Balance should be min $3 or max $100.';
+    //                 \Log::channel('wallet')->info($message);
+    //                 return response()->json(['message' => $message] , 404);   
+    //             }
+                
+    //         }else{
+    //             $message = 'Record not found.';
+    //             \Log::channel('wallet')->info($message);
+    //             return response()->json(['message' => $message] , 404);
+    //         }
+    //     }catch(Exception $e){
+    //         $message = 'Something went wrong!';
+    //         \Log::channel('wallet')->info($message);
+    //         return response()->json(['error'=> $message] , 500);
+    //     }
+    // }
     public function deposite(Request $request){
         $validator = Validator::make($request->all(), [
             'owner_id' => 'required | numeric',
@@ -37,7 +69,8 @@ class UsersController extends Controller{
             $user = User::find($request->owner_id);  
             if($user){
                 if($request->balance >= 3 && $request->balance <= 100 ) {                    
-                    $user->wallet->deposit($request->balance); 
+                    $user->wallet = $request->balance;
+                    $user->save(); 
                     return response()->json(['message' => 'Success! $'.$request->balance.' added to your wallet'] , 404);
                 }else{
                     $message = 'Balance should be min $3 or max $100.';
@@ -56,7 +89,6 @@ class UsersController extends Controller{
             return response()->json(['error'=> $message] , 500);
         }
     }
-
     /**
      * Buy Cookies money
      * @request
@@ -74,14 +106,14 @@ class UsersController extends Controller{
             $user = User::find($request->user_id);  
             if($user){
                 //check min balnce to buy cookie
-                $balance = $user->wallet->balance;
+                $balance = $user->wallet;
                 if($balance < 1 && $request->quanity > $balance){
                     $message = 'Not enough balance to buy a cookie';
                     \Log::channel('wallet')->info($message);
                     return response()->json(['message' => $message] , 422);   
                 }else{
-                    $user->wallet->withdraw($request->quanity);
-                    $balance = $user->wallet->balance;
+                    $user->wallet = $user->wallet - $request->quanity;
+                    $balance = $user->wallet;
                     $message =  'Succefully! Buy '. $request->quanity .' Cookies. Reaming balance is '. $balance; 
                     return response()->json(['message' => $message] , 200);    
                 }             
